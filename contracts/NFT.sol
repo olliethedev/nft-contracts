4c9770d84b719260ef65d323ac7431becfb793b9
olliethedev <3martynov@gmail.com>
2022-01-24 16:16:19 -0500

    work

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -10,39 +10,24 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 contract NFT is ERC721, Ownable, ReentrancyGuard {
   using Counters for Counters.Counter;
   using SafeMath for uint256;
+  using Strings for uint256;
   Counters.Counter private _tokenIds;
   uint256 private _mintCost;
   uint256 private _maxSupply;
-  bool private _isPublicMintEnabled;
+  string private _baseURL;
+  uint256 private FEE_DENOMINATOR = 1000;
+  uint256 private FEE_PLATFORM_NUMERATOR = 50;
+  uint256 private FEE_OWNER_NUMERATOR = 950;
+  address private PLATFORM_FEE_ADDRESS = 0x0000000000000000000000000000000000000000;
   
   /**
   * @dev Initializes the contract setting the `tokenName` and `symbol` of the nft, `cost` of each mint call, and maximum `supply` of the nft.
   * Note: `cost` is in wei. 
   */
-  constructor(string memory tokenName, string memory symbol, uint256 cost, uint256 supply) ERC721(tokenName, symbol) Ownable() {
+  constructor(string memory tokenName, string memory symbol, uint256 cost, uint256 supply, string memory baseURL) ERC721(tokenName, symbol) Ownable() {
     _mintCost = cost;
     _maxSupply = supply;
-    _isPublicMintEnabled = false;
-  }
-
-  /**
-  * @dev Changes contract state to enable public access to `mintTokens` function
-  * Can only be called by the current owner.
-  */
-  function allowPublicMint()
-  public
-  onlyOwner{
-    _isPublicMintEnabled = true;
-  }
-
-  /**
-  * @dev Changes contract state to disable public access to `mintTokens` function
-  * Can only be called by the current owner.
-  */
-  function denyPublicMint()
-  public
-  onlyOwner{
-    _isPublicMintEnabled = false;
+    _baseURL = baseURL;
   }
 
   /**
@@ -53,30 +38,31 @@ contract NFT is ERC721, Ownable, ReentrancyGuard {
   public
   payable
   nonReentrant{
-    require(_isPublicMintEnabled, "Mint disabled");
     require(count > 0 && count <= 100, "You can drop minimum 1, maximum 100 NFTs");
-    require(count.add(_tokenIds.current()) < _maxSupply, "Exceeds max supply");
+    require(count.add(_tokenIds.current()) < (_maxSupply+1), "Exceeds max supply");
     require(owner() == msg.sender || msg.value >= _mintCost.mul(count),
            "Ether value sent is below the price");
+    
     for(uint i=0; i<count; i++){
         _mint(msg.sender);
-     }
+    }
+    payable(owner()).transfer(msg.value.mul(FEE_OWNER_NUMERATOR).div(FEE_DENOMINATOR));
+    payable(PLATFORM_FEE_ADDRESS).transfer(msg.value.mul(FEE_PLATFORM_NUMERATOR).div(FEE_DENOMINATOR));
   }
 
-  /**
+   /**
   * @dev Mint a token to each Address of `recipients`.
   * Can only be called if requirements are satisfied.
   */
-  function mintTokens(address[] calldata recipients)
+  function adminMintTokens(address[] calldata recipients)
   public
   payable
+  onlyOwner
   nonReentrant{
     require(recipients.length>0,"Missing recipient addresses");
-    require(owner() == msg.sender || _isPublicMintEnabled, "Mint disabled");
     require(recipients.length > 0 && recipients.length <= 100, "You can drop minimum 1, maximum 100 NFTs");
-    require(recipients.length.add(_tokenIds.current()) < _maxSupply, "Exceeds max supply");
-    require(owner() == msg.sender || msg.value >= _mintCost.mul(recipients.length),
-           "Ether value sent is below the price");
+    require(recipients.length.add(_tokenIds.current()) < (_maxSupply+1), "Exceeds max supply");
+    
     for(uint i=0; i<recipients.length; i++){
         _mint(recipients[i]);
      }
@@ -99,14 +85,6 @@ contract NFT is ERC721, Ownable, ReentrancyGuard {
   }
 
   /**
-  * @dev Transfers contract balance to contract owner.
-  * Can only be called by the current owner.
-  */
-  function withdraw() public onlyOwner{
-    payable(owner()).transfer(address(this).balance);
-  }
-
-  /**
   * @dev Used by public mint functions and by owner functions.
   * Can only be called internally by other functions.
   */
@@ -121,19 +99,28 @@ contract NFT is ERC721, Ownable, ReentrancyGuard {
   function getCost() public view returns (uint256){
     return _mintCost;
   }
+
   function totalSupply() public view returns (uint256){
     return _maxSupply;
   }
+
   function getCurrentSupply() public view returns (uint256){
     return _tokenIds.current();
   }
-  function getMintStatus() public view returns (bool) {
-    return _isPublicMintEnabled;
+
+  function _baseURI() override internal view returns (string memory) {
+    return _baseURL;
   }
-  function _baseURI() override internal pure returns (string memory) {
-    return "https://mw9spidhbc.execute-api.us-east-1.amazonaws.com/dev/token/trick-or-nft/";
+
+  function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+    string memory baseURI = _baseURI();
+    return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), "/metadata.json")) : "";
   }
-  function contractURI() public pure returns (string memory) {
-    return "https://mw9spidhbc.execute-api.us-east-1.amazonaws.com/dev/contract/trick-or-nft";
+
+  function contractURI() public view returns (string memory) {
+    string memory baseURI = _baseURI();
+    return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, "metadata.json")) : "";
   }
 }
